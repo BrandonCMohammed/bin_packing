0fa1d085b6698affc8a4d1a94d2e0ae8d784a2fa
BrandonCMohammed <64518176+BrandonCMohammed@users.noreply.github.com>
2022-04-06 13:18:03 -0400

    added annotations to half of the program

--- bin packing.py
+++ bin packing.py
@@ -1,37 +1,57 @@
-import numpy as np
+import numpy as np #Numpy is a useful library for number manipulation
 
-num_items = 15
+#The number of items to be stored
+num_items = 15 
+
+#Since we don't have specific numbers for the weights, a random set of integers will be generated in the range of 3 to 7
+#*item_weight_range = 3 7, it does not including 7
 item_weight_range = [3, 7]
 weights = list(np.random.randint(*item_weight_range, num_items))
+
+#We will now establish the bin capacity, We do not have a specific number right now so the mean of the weights will be found,
+#Then multiplied by 10 and casted to an int to remove any decimal points.
 bin_capacity = int(10 * np.mean(weights))
 print("Problem: pack a total weight of {} into bins of capacity {}.".format(sum(weights), bin_capacity)) 
 
+
+#Instantiating our CQM
 from dimod import ConstrainedQuadraticModel
 cqm = ConstrainedQuadraticModel()
 
+#Now we can create our objective function, The formula will be in the pdf
+#Binary is used to create BinaryQuadraticModules
 from dimod import Binary
+
+#This creates a list of 
+#BQMs with linear keys of bin_used_{j} and values of 1
 bin_used = [Binary(f'bin_used_{j}') for j in range(num_items)]
+#print(bin_used[0])
 
 cqm.set_objective(sum(bin_used))
 
-bin_used[0]
+#Constrains
 
-2*bin_used[0]
-
-bin_used[0]*bin_used[1] 
-
+#Now we are creating the first constrain. Each item can go in one bin only. This creates a list of 
+#BQMs with linear keys of item_{i}_in_bin_{j} and values of 1 
 item_in_bin = [[Binary(f'item_{i}_in_bin_{j}') for j in range(num_items)] for i in range(num_items)]
+#print(item_in_bin[0])
 
+#for each item i in item in bin, we add the constraint
 for i in range(num_items):
     one_bin_per_item = cqm.add_constraint(sum(item_in_bin[i]) == 1, label=f'item_placing_{i}')
+    #print(one_bin_per_item)
 
+#This is constraint two. 
 for j in range(num_items):
     bin_up_to_capacity = cqm.add_constraint(
         sum(weights[i] * item_in_bin[i][j] for i in range(num_items)) - bin_used[j] * bin_capacity <= 0,
         label=f'capacity_bin_{j}')
 
+    print(bin_up_to_capacity)
+
 len(cqm.variables)
 
+"""
 from dwave.system import LeapHybridCQMSampler
 sampler = LeapHybridCQMSampler()  
 
@@ -59,3 +79,4 @@ for bin in selected_bins:
     b = get_indices(in_bin[0])[1]
     w = [weights[get_indices(item)[0]] for item in in_bin]
     print("Bin {} has weights {} for a total of {}.".format(b, w, sum(w)))
+"""
